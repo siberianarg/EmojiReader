9321a9f995d203537254bffdbab99a39498a9427
Zhan <galymzhan.aitkaliyev@gmail.com>
2023-04-28 01:43:40 +0600

    created func favouriteAction

--- EmojiReader/EmojiTableViewController.swift
+++ EmojiReader/EmojiTableViewController.swift
@@ -72,12 +72,13 @@ class EmojiTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         let done = doneAction(at: indexPath)
-        return UISwipeActionsConfiguration(actions: [done])
+        let favoutite = favouriteAction(at: indexPath)
+        return UISwipeActionsConfiguration(actions: [favoutite, done])
     }
     
     //create Action
     func doneAction(at indexPath: IndexPath) -> UIContextualAction {
-        let action = UIContextualAction(style: .destructive, title: "Done") { (action, view, completion) in
+        let action = UIContextualAction(style: .destructive, title: "Done") { action, view, completion in
             self.objects.remove(at: indexPath.row)
             self.tableView.deleteRows(at: [indexPath], with: .automatic)
             completion(true)
@@ -85,7 +86,19 @@ class EmojiTableViewController: UITableViewController {
         action.backgroundColor = .systemGreen
         action.image = UIImage(systemName: "checkmark.circle.fill")
         return action
+    }
+    
+    func favouriteAction(at indexPath: IndexPath) -> UIContextualAction {
         
+        var object = objects[indexPath.row]
+        let action = UIContextualAction(style: .normal, title: "Favourite") { action, view, completion in
+            object.isFavourite = !object.isFavourite
+            self.objects[indexPath.row] = object
+            completion(true)
+        }
+        action.backgroundColor = object.isFavourite ? .systemPink : .systemGray
+        action.image = UIImage(systemName: "heart")
+        return action
     }
     /*
      // Override to support conditional editing of the table view.
