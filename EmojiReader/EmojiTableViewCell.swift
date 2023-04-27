0bd05f9b037ad786c91e2af2103c30580e1363d7
Zhan <galymzhan.aitkaliyev@gmail.com>
2023-04-27 19:00:04 +0600

    created class EmojiTableViewCell with 3 outlets deleted register with "cell" identifier in viewDidLoad cause we did it in mainStoryboard changed withIdentifier to "emojiCell"

---
+++ EmojiReader/EmojiTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiReader
+//
+//  Created by Perizat Omar on 27.04.2023.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var emojiLabel: UILabel!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
--- EmojiReader/EmojiTableViewController.swift
+++ EmojiReader/EmojiTableViewController.swift
@@ -3,7 +3,7 @@
 //  EmojiReader
 //
 //  Created by Perizat Omar on 26.04.2023.
-//
+// 
 
 import UIKit
 
@@ -19,7 +19,6 @@ class EmojiTableViewController: UITableViewController {
         self.title = "Emoji Reader"
         self.navigationItem.leftBarButtonItem = self.editButtonItem
         
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         
     }
     
@@ -37,8 +36,8 @@ class EmojiTableViewController: UITableViewController {
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text =  "\(indexPath)"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "emojiCell", for: indexPath) as! EmojiTableViewCell
+        cell.emojiLabel.text = "😃"
         
         return cell
     }
