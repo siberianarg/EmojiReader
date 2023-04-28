bf1c659b944c0b5e735f14f2afe24334b8c7c89e
Zhan <galymzhan.aitkaliyev@gmail.com>
2023-04-28 17:40:23 +0600

    set prepare for segue from EmojiTVC created updateUI func in newEmojiTVC

--- EmojiReader/NewEmojiTableViewController.swift
+++ EmojiReader/NewEmojiTableViewController.swift
@@ -20,7 +20,8 @@ class NewEmojiTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.title = "Emoji Edit"
+        updateUI()
+        
         updateSaveButtonState()
     }
     
@@ -30,6 +31,12 @@ class NewEmojiTableViewController: UITableViewController {
         let descriptionText = descriptionTextField.text ?? ""
         
         saveButton.isEnabled = !emojiText.isEmpty && !nameText.isEmpty && !descriptionText.isEmpty
+    }
+    
+    private func updateUI() {
+        emojiTextField.text = emoji.emoji
+        nameTextField.text = emoji.name
+        descriptionTextField.text = emoji.description 
     }
     
     @IBAction func textChanged(_ sender: UITextField) {
